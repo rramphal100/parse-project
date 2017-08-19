75af9b5cb7ca005fa8c36eb49ac083a392100b23
Ryan Ramphal <rramphal94@gmail.com>
2017-08-19 02:37:45 -0400

    Initial commit. First attempt at minimum working project. Includes Parse server, Parse dashboard, and usage of commander to use CLI options for custom parse environments from the same code.

---
+++ app.js
@@ -0,0 +1,48 @@
+var dotenv = require('dotenv').config();
+var commander = require('commander');
+var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
+var app = require('express')();
+
+var allowInsecureHTTP = !process.env.NODE_ENV == 'production' || false;
+
+commander
+	.version('1.0.0')
+	.usage('[options]')
+	.option('--dburl', 'Database URL')
+	.option('--masterkey', 'Parse Master Key')
+	.option('--cloudpath', 'Parse Cloud Code Path')
+	.option('--appid', 'Parse App ID')
+	.option('--parseurl', 'Parse Server URL')
+	.option('--appname', 'Parse Server App Name (appears on Parse dashboard)')
+	.parse(process.argv);
+
+var parse = new ParseServer({
+  databaseURI: commander.dburl || process.env.DATABASE_URL || 'mongodb://localhost:27017/', // Connection string for your MongoDB database
+  //cloud: commander.cloudpath || process.env.CLOUD_CODE_PATH || '/cloud/main.js', // Absolute path to your Cloud Code
+  appId: commander.appid || process.env.PARSE_APP_ID || 'devserver1',
+  masterKey: commander.masterkey || process.env.PARSE_MASTER_KEY || 'oanvAWrvaRVaFVASERAdfBSdfBSRTsrteYnSTbserVS', // Keep this key secret!
+  //fileKey: 'optionalFileKey',
+  serverURL: commander.parseurl || process.env.PARSE_SERVER_URL || 'http://localhost:1337/parse' // Don't forget to change to https if needed
+});
+
+var dashboard = new ParseDashboard({
+	"apps": [
+			{	
+				"serverURL": commander.parseurl || process.env.PARSE_SERVER_URL || "http://localhost:1337/parse",
+				"appId": commander.appid || process.env.PARSE_APP_ID || "devserver1",
+				"masterKey": commander.masterkey || process.env.PARSE_MASTER_KEY || "oanvAWrvaRVaFVASERAdfBSdfBSRTsrteYnSTbserVS",
+				"appName": commander.appname || process.env.PARSE_APP_NAME || "dev1"
+			}
+		]
+}, allowInsecureHTTP);
+
+// Serve the Parse API on the /parse URL prefix
+app.use('/parse', parse);
+
+// Serve the Parse Dashboard on the /dashboard URL prefix
+app.use('/dashboard', dashboard);
+
+app.listen(1337, function() {
+  console.log('Ryan\'s Parse Project running on port 1337.')
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+*Ryan's Parse Project*
+
+This project is my attempt to run a MongoDB server and manage it with Parse server and a Parse Dashboard that are both hosted from a single node express application.
